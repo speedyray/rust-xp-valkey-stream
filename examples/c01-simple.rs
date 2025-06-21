5fbc1f76b51470a8ad631c0deda409f242f59d61
Raynold Gyasi <speedyray2ray@gmail.com>
2025-06-20 20:10:57 -0400

    completed simple.rs

--- examples/c01-simple.rs
+++ examples/c01-simple.rs
@@ -1,5 +1,21 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello World");
+use redis::Commands;
 
-    Ok(())
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Hello, World");
+	// connect to redis
+	// or "redis://127.0.0.1:6379" (to specify port when not default)
+	let client = redis::Client::open("redis://127.0.0.1/")?;
+	let mut con = client.get_connection()?;
+
+	// throw away the result, just make sure it does not fail
+	let _: () = con.set("my_key", 42)?;
+	// read back the key and return it.  Because the return value
+	// from the function is a result for integer this will automatically
+	// convert into one.
+	let res: i32 = con.get("my_key")?;
+
+	println!("->>: {res}");
+
+	Ok(())
 }
